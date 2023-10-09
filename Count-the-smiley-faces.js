cd82513217d41f06a235e93ed3fef03aea99d38a
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-10-09 14:38:51 -0700

    Update Count-the-smiley-faces.js

--- Count-the-smiley-faces.js
+++ Count-the-smiley-faces.js
@@ -25,3 +25,14 @@ Note:
 
 
 // Solution
+
+function countSmileys(arr) {
+  let smileys = 0;
+  let validSmileys = [":D", ";D", ":)", ";)", ":-D", ";-D", ":-)", ";-)", ":~D", ";~D", ":~)", ";~)"];
+  for (let i = 0; i < arr.length; i++) {
+    if (validSmileys.includes(arr[i])) {
+      smileys++;
+    }
+  }
+  return smileys;
+}
